026076d9778cf63e7e066baaea6d8af31bb9bc8a
yanchen <87835546@qq.com>
2019-08-13 10:22:58 +0800

    1

---
+++ unpackage/dist/dev/mp-weixin/node-modules/@dcloudio/uni-ui/lib/uni-collapse/uni-collapse 2.js
@@ -0,0 +1,24 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["node-modules/@dcloudio/uni-ui/lib/uni-collapse/uni-collapse"],{
+
+/***/ 122:
+/*!********************************************************************************************************************!*\
+  !*** /Users/yancheng/Documents/GitHub/uniapp-demo/node_modules/@dcloudio/uni-ui/lib/uni-collapse/uni-collapse.vue ***!
+  \********************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\nError: ENOENT: no such file or directory, open '/Users/yancheng/Documents/GitHub/uniapp-demo/node_modules/@dcloudio/uni-ui/lib/uni-collapse/uni-collapse.vue'");
+
+/***/ })
+
+}]);
+//# sourceMappingURL=../../../../../../.sourcemap/mp-weixin/node-modules/@dcloudio/uni-ui/lib/uni-collapse/uni-collapse.js.map
+;(global["webpackJsonp"] = global["webpackJsonp"] || []).push([
+    'node-modules/@dcloudio/uni-ui/lib/uni-collapse/uni-collapse-create-component',
+    {
+        'node-modules/@dcloudio/uni-ui/lib/uni-collapse/uni-collapse-create-component':(function(module, exports, __webpack_require__){
+            __webpack_require__('1')['createComponent'](__webpack_require__(122))
+        })
+    },
+    [['node-modules/@dcloudio/uni-ui/lib/uni-collapse/uni-collapse-create-component']]
+]);                
