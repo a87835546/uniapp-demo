9043d97bc37ed062ddbf949884ccb0baac6e9505
yicen <87835546@qq.com>
2019-08-14 17:07:59 +0800

    删除

--- unpackage/dist/dev/app-plus/pages/index/map/map.js
+++
@@ -1,2 +0,0 @@
-setCssToHead(["wx-map { width: ",[0,750],"; height: ",[0,750],"; }\nwx-button { z-index: 9999; }\nwx-image { width: 100%; }\n",],undefined,{path:"./pages/index/map/map.wxss"})();
-document.dispatchEvent(new CustomEvent("generateFuncReady", { detail: { generateFunc: $gwx('./pages/index/map/map.wxml') } }));
