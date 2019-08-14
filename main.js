fa25c2f70b0eb8c51f1605abe661bf71c195afba
Noah Li <lishi@onething.com>
2019-08-14 17:50:47 +0800

    删除弃用的控件

--- main.js
+++ main.js
@@ -2,25 +2,11 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import App from './App'
 import {post,get} from './pages/utils/httpRequest.js'
-// #ifndef APP-PLUS
-import VCalendar from 'v-calendar'
-Vue.use(VCalendar, {
-  formats: {
-    title: 'MMMM YYYY',
-    weekdays: 'W',
-    navMonths: 'MMM',
-    input: ['L', 'YYYY-MM-DD', 'YYYY/MM/DD'],
-    dayPopover: 'L',
-  }
-})
-// #endif
 
 Vue.config.productionTip = false
 Vue.prototype.post = post
 Vue.prototype.get = get
 App.mpType = 'app'
-
-
 
 const app = new Vue({
     ...App
