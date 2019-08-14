acc40996fa8b007a12acaad10c3c394c34b59ae9
yicen <87835546@qq.com>
2019-08-14 16:05:50 +0800

    1

--- unpackage/dist/dev/app-plus/pages/index/refresh/refresh.js
+++ unpackage/dist/dev/app-plus/pages/index/refresh/refresh.js
@@ -1,6 +1,2 @@
-<<<<<<< HEAD
-setCssToHead(["wx-view.",[1],"data-v-1258f951 { height: 300px; width: 100vw; }\nwx-view .",[1],"page.",[1],"data-v-1258f951 { height: 100vw; }\n",],undefined,{path:"./pages/index/refresh/refresh.wxss"})();
-=======
 setCssToHead(["wx-view.",[1],"data-v-1aba9248 { height: 300px; width: 100vw; }\nwx-view .",[1],"page.",[1],"data-v-1aba9248 { height: 100vw; }\n",],undefined,{path:"./pages/index/refresh/refresh.wxss"})();
->>>>>>> yanchen
 document.dispatchEvent(new CustomEvent("generateFuncReady", { detail: { generateFunc: $gwx('./pages/index/refresh/refresh.wxml') } }));
