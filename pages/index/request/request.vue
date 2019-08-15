fe2f979243177e9254e086a60841ebd8170f9fd9
yanchen <87835546@qq.com>
2019-08-15 21:49:22 +0800

    测试网络请求

--- pages/index/request/request.vue
+++ pages/index/request/request.vue
@@ -13,7 +13,7 @@
 		},
 		//封装的网络请求类似于iOS中的AFNetworking ，并且把请求方法挂载到全局
 		onLoad:function(e){
-			this.post('https://www.baidu.com').then(result=>{
+			this.post('http://120.77.85.169:8082').then(result=>{
 				console.log(result);
 			}).catch(e=>{
 				console.log(e);
